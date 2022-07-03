63fd541e5c88861a34de60552517fa512bac2e03
Margarita Gediun <margarita.gediun@gmail.com>
2022-07-03 21:24:26 +0200

    feat: added task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n > 0) {
+        let input = n;
+        let arr = (`${input}`.split('').map(Number));
+        let arrRevers = arr.reverse();
+        let resalt = +arrRevers.join("")
+        return resalt;
+    } else {
+        let input = n;
+        let arr = (`${input}`.split('').map(Number));
+        let arrRevers = arr.reverse();
+        arrRevers.pop();
+        let resalt = +arrRevers.join("");
+        return resalt;
+    }
 }
